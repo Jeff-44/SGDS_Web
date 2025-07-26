86a7ec0a61ceabfb4c70b2ae7bffdad2c8cbca0d
Jeff FORESTAL <jforestal@solutions.ht>
2025-07-26 11:24:18 -0400

    Add project files.

---
+++ src/Infrastructure/DataAccess/SGDSDbConctext.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataAccess
+{
+    public class SGDSDbConctext : IdentityDbContext<ApplicationUser>
+    {
+        public SGDSDbConctext(DbContextOptions<SGDSDbConctext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        
+    }
+}
